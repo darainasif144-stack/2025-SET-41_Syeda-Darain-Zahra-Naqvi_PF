5a75106ef4a149a7ed3f3dc48aca14a786d2aa0d
darainasif144-stack <darainasif144@gmail.com>
2026-01-15 19:39:47 +0500

    Add files via upload

---
+++ pf lab 10(1).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cout<<"First ten Natural Number"<<endl;
+    n = 1;
+    while(n <=10 )
+    {
+        cout<<n<<"  ";
+        n++;
+    }
+
+    return 0;
+}
